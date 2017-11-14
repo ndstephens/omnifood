4fbe27c068971ba896ab64d92d3cf0826c5d8b30
Nathaniel Stephens <ndstephens@gmail.com>
2017-11-13 19:49:04 -0500

    Adds mobile navigation, CSS reformatting

--- resources/js/script.js
+++ resources/js/script.js
@@ -14,6 +14,22 @@ $(document).ready(function() {
     }
   );
 
+  //? ----- MOBILE NAVIGATION -----
+  $('.js--nav-icon').click(function(e) {
+    e.preventDefault();
+    var nav = $('.js--main-nav');
+    var icon = $('.js--nav-icon i');
+    nav.slideToggle(200);
+
+    if (icon.hasClass('ion-navicon-round')) {
+      icon.addClass('ion-close-round');
+      icon.removeClass('ion-navicon-round');
+    } else {
+      icon.removeClass('ion-close-round');
+      icon.addClass('ion-navicon-round');
+    }
+  });
+
   //? ----- HERO BUTTONS -----
   $('.js--scroll-to-plans').click(function() {
     $('html, body').animate(
